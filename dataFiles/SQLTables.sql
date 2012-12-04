196baa82c0a6bc6596fb47c5be9fdc3db9156e75
Diego Gomes <dgs_gomes_silva@hotmail.com>
2012-12-04 11:59:39 -0200

    Arquivo SQL Atualizado

--- dataFiles/SQLTables.sql
+++ dataFiles/SQLTables.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 29/11/2012 às 11h53min
+-- Tempo de Geração: 04/12/2012 às 11h58min
 -- Versão do Servidor: 5.5.28
 -- Versão do PHP: 5.3.10-1ubuntu3.4
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `acos` (
   KEY `idx_acos_lft_rght` (`lft`,`rght`),
   KEY `idx_acos_alias` (`alias`),
   KEY `idx_acos_model_foreign_key` (`model`,`foreign_key`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=183 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=201 ;
 
 -- --------------------------------------------------------
 
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `aros_acos` (
   UNIQUE KEY `ARO_ACO_KEY` (`aro_id`,`aco_id`),
   UNIQUE KEY `idx_aros_acos_aro_id_aco_id` (`aro_id`,`aco_id`),
   KEY `aco_id` (`aco_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=82 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=94 ;
 
 -- --------------------------------------------------------
 
@@ -128,6 +128,25 @@ CREATE TABLE IF NOT EXISTS `images` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `parts`
+--
+
+CREATE TABLE IF NOT EXISTS `parts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
+  `posicao` varchar(20) NOT NULL,
+  `seguranca` tinyint(1) NOT NULL DEFAULT '0',
+  `codigo` varchar(20) NOT NULL,
+  `nome` text NOT NULL,
+  `exibe` tinyint(1) NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=68 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `posts`
 --
 
@@ -164,14 +183,18 @@ CREATE TABLE IF NOT EXISTS `types` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` char(40) NOT NULL,
   `group_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `tokenhash` text NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -186,6 +209,43 @@ CREATE TABLE IF NOT EXISTS `widgets` (
   `quantity` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `workshops`
+--
+
+CREATE TABLE IF NOT EXISTS `workshops` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(10) NOT NULL,
+  `consultor` varchar(25) NOT NULL,
+  `cnpj` varchar(30) NOT NULL,
+  `ie` varchar(30) NOT NULL,
+  `razaosocial` text NOT NULL,
+  `rua` text NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `complemento` varchar(50) NOT NULL,
+  `bairro` varchar(30) NOT NULL,
+  `cidade` varchar(30) NOT NULL,
+  `uf` varchar(3) NOT NULL,
+  `cep` varchar(20) NOT NULL,
+  `ddd` varchar(3) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `ddd2` varchar(3) NOT NULL,
+  `telefone2` varchar(15) NOT NULL,
+  `ddd3` varchar(3) NOT NULL,
+  `telefone3` varchar(15) NOT NULL,
+  `email` text NOT NULL,
+  `responsavel` varchar(25) NOT NULL,
+  `proprietario` varchar(25) NOT NULL,
+  `tecnico` varchar(25) NOT NULL,
+  `skype` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=1 ;
 
 --
 -- Restrições para as tabelas dumpadas
